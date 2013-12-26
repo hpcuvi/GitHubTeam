f68523ea05ac1899b92f468145a0632095b79d72
Pato <hpcuvi@gmail.com>
2013-12-26 10:01:05 -0500

    Initial Commit

---
+++ GitSource/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitSource
+//
+//  Created by curso on 26/12/13.
+//  Copyright (c) 2013 anyway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitSource/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitSource
+//
+//  Created by curso on 26/12/13.
+//  Copyright (c) 2013 anyway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
